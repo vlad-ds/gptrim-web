5807b303b1542cafe7c7a0a2d86244773a1c0d53
Vlad Gheorghe <contact@vlad-gheorghe.com>
2023-04-15 15:07:59 +0200

    Keep negation words

--- gptrim.py
+++ gptrim.py
@@ -5,22 +5,24 @@ from nltk.stem import PorterStemmer
 nltk.download('punkt')
 nltk.download('stopwords')
 
-
-def remove_articles_prepositions(words):
-    articles_prepositions = [
-        'the', 'a', 'an', 'in', 'on', 'at', 'for', 'to', 'of'
-    ]
-    return [word for word in words if word not in articles_prepositions]
+negation_words = ['no', 'nor', 'not', 'don', "don't", 'ain', 'aren',
+                  "aren't", 'couldn', "couldn't", 'didn', "didn't",
+                  'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
+                  "hasn't", 'haven', "haven't", 'isn', "isn't",
+                  'mightn', "mightn't", 'mustn', "mustn't",
+                  'needn', "needn't", 'shan', "shan't", 'shouldn',
+                  "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
+                  'won', "won't", 'wouldn', "wouldn't"]
 
 
 def gptrim(text: str):
 
     words = nltk.word_tokenize(text)
     stop_words = stopwords.words("english")
-    words = [word for word in words if word not in stop_words]
+    stop_words_selected = [word for word in stop_words if word not in negation_words]
+    words = [word for word in words if word not in stop_words_selected]
 
     ps = PorterStemmer()
     words = [ps.stem(word) for word in words]
-    words = remove_articles_prepositions(words)
     trimmed = "".join(words)
     return trimmed
